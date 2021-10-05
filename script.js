1d5703318054f5314a5bfb869fb2dd5e265ea113
Kiri <kirilynne@gmail.com>
2021-10-05 16:20:00 -0400

    click the drink function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var nameInputEl = $('#drinkInput');
 var ingredientInputEl = $('#ingredientInput');
 var submitBtnEl = $('#drinkSubmit');
 var switchBtnEl = $('#switchBtn');
+var drinkChoiceEl = $('<a>')
 
 function submitDrink(event) {
     event.preventDefault()
@@ -13,9 +14,9 @@ function submitDrink(event) {
 
     if (drink !== "") {
         var searchCocktailByNameAPI = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
-        fetch(searchCocktailByNameAPI).then(function(response) {
+        fetch(searchCocktailByNameAPI).then(function (response) {
             if (response.ok) {
-                response.json().then(function(data) {
+                response.json().then(function (data) {
                     console.log(data);
                     displayDrinks(data);
                 })
@@ -27,9 +28,9 @@ function submitDrink(event) {
 
     if (ingredient !== "") {
         var searchCocktailByIngredientAPI = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient;
-        fetch(searchCocktailByIngredientAPI).then(function(response) {
+        fetch(searchCocktailByIngredientAPI).then(function (response) {
             if (response.ok) {
-                response.json().then(function(data) {
+                response.json().then(function (data) {
                     console.log(data);
                     displayDrinks(data);
                 })
@@ -47,9 +48,9 @@ function displayDrinks(data) {
 
     for (var i = 0; i < drinks.length; i++) {
         console.log(drinks[i]);
-        var card = $('<div>');
+        var card = $('<a>')
         card.addClass("card");
-        card.css({"flex": "1 0 300px", "margin":"10px"})
+        card.css({ "flex": "1 0 300px", "margin": "10px" })
         var cardHeader = $('<div>');
         cardHeader.addClass("card-divider");
         var cardImg = $('<img>');
@@ -58,7 +59,7 @@ function displayDrinks(data) {
 
         cardHeader.text(drinks[i].strDrink);
         cardImg.attr("src", drinks[i].strDrinkThumb);
-        
+
         cardSection.append('<h4><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></h4>');
         card.append(cardHeader);
         card.append(cardImg);
@@ -71,18 +72,24 @@ function displayDrinks(data) {
 function switchInput(event) {
     event.preventDefault();
     if (ingredientInputEl.css("display") === "none") {
-        nameInputEl.css({"display": "none"});
-        ingredientInputEl.css({"display": "block"});
+        nameInputEl.css({ "display": "none" });
+        ingredientInputEl.css({ "display": "block" });
         switchBtnEl.text("Search by Drink");
         ingredientInputEl.val('');
 
     } else if (nameInputEl.css("display") === "none") {
-        nameInputEl.css({"display": "block"});
-        ingredientInputEl.css({"display": "none"});
+        nameInputEl.css({ "display": "block" });
+        ingredientInputEl.css({ "display": "none" });
         switchBtnEl.text("Search by Ingredient");
         nameInputEl.val('');
     }
 }
+
+function drinkRating() {
+    console.log(hey)
+}
+
+drinkChoiceEl.on("click", drinkRating);
 
 switchBtnEl.on("click", switchInput);
 submitBtnEl.on("click", submitDrink);
