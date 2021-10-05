518de5141b995d91a7db1b5a7e431301eb160145
Andres Liu <andres.liu22@gmail.com>
2021-10-05 12:17:37 -0700

    Display drink results from search

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var submitBtnEl = $('#drinkSubmit');
 function submitDrink(event) {
     event.preventDefault()
 
-
     var drink = nameInputEl.val();
     var ingredient = ingredientInputEl.val();
 
@@ -16,9 +15,11 @@ function submitDrink(event) {
             if (response.ok) {
                 response.json().then(function(data) {
                     console.log(data);
+                    displayDrinks(data);
                 })
             }
         })
+        nameInputEl.val('');
     }
 
     if (ingredient !== "") {
@@ -27,14 +28,40 @@ function submitDrink(event) {
             if (response.ok) {
                 response.json().then(function(data) {
                     console.log(data);
+                    displayDrinks(data);
                 })
             }
         })
+        ingredientInputEl.val('');
     }
-
-    
 
 }
 
+function displayDrinks(data) {
+
+    var drinks = data.drinks;
+
+    for (var i = 0; i < drinks.length; i++) {
+        console.log(drinks[i]);
+        var card = $('<div>');
+        card.addClass("card");
+        card.css({"width": "300px", "margin":"10px"})
+        var cardHeader = $('<div>');
+        cardHeader.addClass("card-divider");
+        var cardImg = $('<img>');
+        var cardSection = $('<div>');
+        cardSection.addClass("card-section");
+
+        cardHeader.text(drinks[i].strDrink);
+        cardImg.attr("src", drinks[i].strDrinkThumb);
+        
+        cardSection.append('<h4>Rating</h4><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>');
+        card.append(cardHeader);
+        card.append(cardImg);
+        card.append(cardSection);
+
+        $('#drinks-card').append(card);
+    }
+}
 
 submitBtnEl.on("click", submitDrink);
