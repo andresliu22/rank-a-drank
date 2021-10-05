7fd44d12f1e20c7ef6aa6f8c56821f370e14fe81
Andres Liu <andres.liu22@gmail.com>
2021-10-05 11:08:38 -0700

    Get drinks by ingredients

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 var formEl = $('#drinkChoice');
-var inputEl = $('#drinkInput');
+var nameInputEl = $('#drinkInput');
+var ingredientInputEl = $('#ingredientInput');
 var submitBtnEl = $('#drinkSubmit');
-
 
 function submitDrink(event) {
     event.preventDefault()
 
-    var drink = inputEl.val()
 
-    var searchCocktailByNameAPI = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
+    var drink = nameInputEl.val();
+    var ingredient = ingredientInputEl.val();
 
-    fetch(searchCocktailByNameAPI).then(function(response) {
-        if (response.ok) {
-            response.json().then(function(data) {
-                console.log(data);
-            })
-        }
-    })
+    if (drink !== "") {
+        var searchCocktailByNameAPI = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
+        fetch(searchCocktailByNameAPI).then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                })
+            }
+        })
+    }
+
+    if (ingredient !== "") {
+        var searchCocktailByIngredientAPI = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient;
+        fetch(searchCocktailByIngredientAPI).then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                })
+            }
+        })
+    }
+
+    
 
 }
 
