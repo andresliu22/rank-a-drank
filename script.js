3716d405f87eaa6fb9c28aaf40dddc9b3ce07c64
Andres Liu <andres.liu22@gmail.com>
2021-10-05 10:59:18 -0700

    Get drinks from cocktail API by name

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-Javascript: 
+var formEl = $('#drinkChoice');
+var inputEl = $('#drinkInput');
+var submitBtnEl = $('#drinkSubmit');
+
+
+function submitDrink(event) {
+    event.preventDefault()
+
+    var drink = inputEl.val()
+
+    var searchCocktailByNameAPI = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
+
+    fetch(searchCocktailByNameAPI).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            })
+        }
+    })
+
+}
+
+
+submitBtnEl.on("click", submitDrink);
