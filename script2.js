2ebbc2dbb01d5f7982d831f59c8a39a5c1302573
Andres Liu <andres.liu22@gmail.com>
2021-10-06 07:54:32 -0700

    create rating buttons

--- script2.js
+++ script2.js
@@ -1,12 +1,13 @@
-var currentDrinkEl = $('chosen-drink-card')
-var drinkSuggestEl = $('drink-suggest-card')
-var locationSuggestEl = $('location-suggest-card')
+var currentDrinkEl = $('#chosen-drink-card')
+var drinkSuggestEl = $('#drink-suggest-card')
+var locationSuggestEl = $('#location-suggest-card')
+
+$(document).ready(function () {
+    chosenDrink();
+});
 
 function currentDrink(data) {
-
-
     var drinks = data.drinks;
-
     for (var i = 0; i < drinks.length; i++) {
         console.log(drinks[i]);
         var card = $('<div>')
@@ -20,37 +21,50 @@ function currentDrink(data) {
 
         cardHeader.text(drinks[i].strDrink);
         cardImg.attr("src", drinks[i].strDrinkThumb);
+        
+        for (var i=0 ; i<5 ; i++) {
+            var starBtn = $('<button>')
+            starBtn.addClass('starBtn');
+            starBtn.append('<i class="fa fa-star checked"></i>')
+            cardSection.append(starBtn);
 
-        cardSection.append('<h4><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></h4>');
+            starBtn.on("click", rateDrink);
+        }
+        
         card.append(cardHeader);
         card.append(cardImg);
         card.append(cardSection);
 
-        $('#chosen-drink-card').append(card);
+        currentDrinkEl.append(card);
     }
-    }
+}
 
-    chosenDrink();
-
-    function chosenDrink() {
-
-        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+function chosenDrink() {
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
     var drink = hashes[0].split('=');
     var drinkName = drink[1].replace('%20', ' ');
 
-        if (drinkName !== "") {
-            var searchCocktailByNameAPI = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName;
-            fetch(searchCocktailByNameAPI).then(function (response) {
-                if (response.ok) {
-                    response.json().then(function (data) {
-                        console.log(data);
-                        currentDrink(data);
-                    })
-                }
-            })
-        }
+    if (drinkName !== "") {
+        var searchCocktailByNameAPI = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkName;
+        fetch(searchCocktailByNameAPI).then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    currentDrink(data);
+                })
+            }
+        })
     }
+}
 
+function rateDrink(event) {
+    console.log(event.target);
+    console.log("I NEEEEEEEED SOME SLEEEEEEEEEEP")
+}
+
+
+
+//$('.startBtn').on("click", rateDrink);
 
 // function for current drink 
 
